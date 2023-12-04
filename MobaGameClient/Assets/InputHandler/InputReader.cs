777a7a0eda68d270cce0733747becb102070e6a8
Nguyen Quoc Hieu <quochieuforwork@gmail.com>
2023-12-04 17:18:16 +0700

    feat: add input handler

---
+++ MobaGameClient/Assets/InputHandler/InputReader.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : GameInput.IGameplayActions, GameInput.IUIActions
+{
+    private GameInput _gameInput;
+
+    public event Action<Vector2> MoveEvent;
+    public event Action JumpEvent;
+    public event Action PauseEvent;
+    public event Action ResumeEvent;
+    
+    public void Initialize()
+    {
+        _gameInput = new GameInput();
+        _gameInput.Gameplay.SetCallbacks(this);
+        _gameInput.UI.SetCallbacks(this);
+    }
+
+    public void SetGameplay()
+    {
+        _gameInput.Gameplay.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log($"context {context.phase} and value {context.ReadValue<Vector2>()}");
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            JumpEvent?.Invoke();
+        }
+
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            
+        }
+
+        
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnResume(InputAction.CallbackContext context)
+    {
+
+    }
+}
