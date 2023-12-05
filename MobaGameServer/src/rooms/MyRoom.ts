837153b880003d622905ff84226c9439a933a099
Nguyen Quoc Hieu <hieunq@mirabo-global.com>
2023-12-05 11:24:27 +0700

    feat: update server package

--- MobaGameServer/src/rooms/MyRoom.ts
+++ MobaGameServer/src/rooms/MyRoom.ts
@@ -1,11 +1,8 @@
-import { Room, Client } from "colyseus";
-import { MyRoomState, Player } from "./schema/MyRoomState";
-export type Position = {
-  x: number,
-  y: number,
-  z: number
-}
+import { Room, Client } from "@colyseus/core";
+import { MyRoomState } from "./schema/MyRoomState";
+
 export class MyRoom extends Room<MyRoomState> {
+  maxClients = 4;
 
   onCreate (options: any) {
     this.setState(new MyRoomState());
@@ -15,26 +12,10 @@ export class MyRoom extends Room<MyRoomState> {
       // handle "type" message
       //
     });
-
   }
 
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
-
-    this.state.players.set(client.sessionId, new Player());
-
-    // Send welcome message to the client.
-    client.send("welcomeMessage", "Welcome to Colyseus!");
-
-    // Listen to position changes from the client.
-    this.onMessage("position", (client, position: Position) => {
-      console.log(`position x ${position.x}, ${position.y}, ${position.z}`)
-      const player = this.state.players.get(client.sessionId);
-      player.x += position.x;
-      player.y += position.y;
-      player.z += position.z;
-      console.log(`updated position x ${player.x}, ${player.y}, ${player.z}`)
-    });
   }
 
   onLeave (client: Client, consented: boolean) {
--- MobaGameServer/src/rooms/schema/MyRoomState.ts
+++ MobaGameServer/src/rooms/schema/MyRoomState.ts
@@ -1,14 +1,7 @@
-import { MapSchema, Schema, Context, type } from "@colyseus/schema";
+import { Schema, Context, type } from "@colyseus/schema";
 
-export class Player extends Schema {
-  @type("number") x: number = 0;
-  @type("number") y: number = 0;
-  @type("number") z: number = 0;
-}
 export class MyRoomState extends Schema {
 
-  @type({map: Player}) players: MapSchema<Player> = new MapSchema<Player>();
+  @type("string") mySynchronizedProperty: string = "Hello world";
 
 }
-
-
